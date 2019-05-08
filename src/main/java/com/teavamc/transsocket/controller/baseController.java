c64d1c9a4d100f54ce59ad10efab36280b04c862
whj <962488473@qq.com>
2019-05-08 18:52:26 +0800

    加了个按钮，连成功了单片机，更改了服务器的IP地址

--- src/main/java/com/teavamc/transsocket/controller/baseController.java
+++ src/main/java/com/teavamc/transsocket/controller/baseController.java
@@ -10,16 +10,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
  * @date 2019/4/28 21:26
  **/
 @Controller
-@RequestMapping("/base")
+@RequestMapping("base")
 public class baseController {
 
-    String prefix = "/base";
+    String prefix = "base";
 
     /**
         * 基础控制器 底层页面
         * @author 张超 teavamc
         * @date 2019/4/28
-        * @param []
         * @return java.lang.String
         */
     @RequestMapping()
